a658226fb40885aa68a4ecc95877f74ad47205f1
Juan Sosa <juanbsosa01@gmail.com>
2024-02-25 23:56:48 -0600

    groups and classes

--- main.js
+++ main.js
@@ -6,19 +6,26 @@ class Student {
     this._name = name;
     this._lastName = lastName;
     this._age = age;
-    this._grade = null;
+    this._grade = [];
+    this._group = null;
+    this._classes = [];
   }
 
   get studentInfo() {
     return `ID: ${this._id} Name: ${this._name} ${this._lastName}, Age: ${this._age} Grade: ${this._grade}`;
   }
 
-  set grade(grade) {
-    this._grade = grade;
+  addGrade(grade) {
+    this._grade.push(grade);
   }
 
   addClass(className = "") {
+    this._classes.push(className)
     classList.push(className);
+  }
+
+  addGroup(group = "") {
+    this._group = group;
   }
 }
 
@@ -29,14 +36,6 @@ class Class {
     this._id = ++Class.countClasses;
     this._name = name;
     this._studentsList = [];
-  }
-
-  addStudent(student) {
-    studentsList.push(student);
-  }
-
-  setGrade(student, grade) {
-    student.grade = grade;
   }
 }
 
@@ -85,22 +84,37 @@ let chargeData = (id) => {
             <p>${student._name} ${student._lastName}</p>
             <h3>Edad:</h3>
             <p>${student._age}</p>
+            <h3>Grupo:</h3>
+            <p>${student._group}</p>
           </div>
+          <h3>Clases:</h3>
           <div id="grades">
             <div>
-              <p>Class <span>10</span> <span>Estado: APROBADO</span></p>
+              <p>${student._classes}</span></p>
             </div>
           </div>
-            <input type="text" id="classInput" placeholder="Nombre de la clase">
           <div>
-            <button onclick="assignClasses(${student._id})">Agregar</button>
-            <button onclick="deleteStudent(${student._id})">Eliminar estudiante</button>
+            <h3>Agregar Clase</h3>
+            <input type="text" id="classInput" placeholder="Nombre de la clase">
+            <input type="number" id="gradeInput" placeholder="Calificación">
+          </div>
+          <div>
+            <button onclick="assignClasses(${student._id})">Agregar Clase</button>
+            <button onclick="assignGroup(${student._id})">Asignar Grupo</button>
+            <button onclick="deleteStudent(${student._id})">Eliminar alumno</button>
           </div>
         </div>`;
       document.getElementById("containerLeft").innerHTML = studentData;
     }
   }
-  loadClasses();
+  loadClasses(id);
+};
+
+let assignGroup = (studentId) => {
+  const group = prompt("Ingresa el número de grupo");
+  const student = findStudentById(studentId);
+  student.addGroup(group);
+  chargeData(studentId);
 };
   
 let deleteStudent = (id) => {
@@ -156,9 +170,11 @@ let submitForm = (event) => {
   loadStudents();
 };
   
-let loadClasses = () => {
+let loadClasses = (studentId) => {
+  const student = findStudentById(studentId)
   let classHTML = "";
-  for (subject of classList) {
+
+  for (subject of student._classes) {
     classHTML += createClass(subject);
   }
   document.getElementById("grades").innerHTML = classHTML;
@@ -174,11 +190,29 @@ let createClass = (subject) => {
   
 let assignClasses = (studentId) => {
   const className = document.getElementById('classInput').value;
-  const student = findStudentById(studentId);
-  student.addClass(new Class(className));
-  loadClasses();
+  const grade = document.getElementById('gradeInput').value;
+  const student = findStudentById(studentId)
+  const clase = new Class(className);
+  student.addClass(clase);
+  loadClasses(studentId);
 };
   
 const findStudentById = (studentId) => {
   return studentsList.find((student) => student._id === studentId);
 };  
+
+let search = () => {
+  var searchQuery = document.getElementById("searchInput").value;
+
+  var searchQueryLower = searchQuery.toLowerCase();
+
+  for (let student of studentsList) {
+    if (student._name.toLowerCase() === searchQueryLower ||
+      student._lastName.toLowerCase() === searchQueryLower ||
+      (student._name +' '+ student._lastName).toLowerCase() === searchQueryLower ||
+      student._id === parseInt(searchQuery, 10)) {
+    chargeData(student._id);
+    return;
+    }
+  }
+}
